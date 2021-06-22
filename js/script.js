8e3b9e225d5085ead4c9b33461bcac727eeef1ab
Ramil Apaev <mr.apaev@gmail.com>
2021-06-22 22:04:30 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 60000);
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2021-06-11');
+    timer('.timer', '2021-07-11');
     cards();
     calc();
     forms('form', modalTimerId);
